c21516fe34e1ebac97fe33917382b77c312e5e04
ななけ <467392+nanake@users.noreply.github.com>
2022-08-21 23:56:41 +0700

    Add libssh (#186)
    Closes #158
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>
    (cherry picked from commit 843642ae9b5c12dc710b78db771bbd69de076ba9)
    Signed-off-by: rokibhasansagar <10854164+rokibhasansagar@users.noreply.github.com>

--- scripts.d/50-libssh.sh
+++ scripts.d/50-libssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://git.libssh.org/projects/libssh.git"
-SCRIPT_COMMIT="37deed27d66ba342e7e8c0a993bfe519f3967337"
+SCRIPT_COMMIT="ac6d2fad4a8bf07277127736367e90387646363f"
 
 ffbuild_enabled() {
     return 0
@@ -12,6 +12,11 @@ ffbuild_dockerbuild() {
     cd libssh
 
     mkdir build && cd build
+
+    if [[ $TARGET == win* ]]; then
+        export CFLAGS="$CFLAGS -Dgettimeofday=ssh_gettimeofday"
+        export CXXFLAGS="$CFLAGS -Dgettimeofday=ssh_gettimeofday"
+    fi
 
     cmake -GNinja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
         -DBUILD_SHARED_LIBS=OFF \
