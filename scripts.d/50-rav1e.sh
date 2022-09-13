63ff574e06e80b8cebd3d03d91ba6549774a03c2
rokibhasansagar <10854164+rokibhasansagar@users.noreply.github.com>
2022-09-14 02:01:04 +0600

    Fix rav1e Build
    (cherry picked from commit 7d31bb2, bd13b3b, 7bb9176, 8bc7936)
    Signed-off-by: rokibhasansagar <10854164+rokibhasansagar@users.noreply.github.com>

--- scripts.d/50-rav1e.sh
+++ scripts.d/50-rav1e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/xiph/rav1e.git"
-SCRIPT_COMMIT="db7a71ae53a31a60bf31bd0635f46e15bdcc444c"
+SCRIPT_COMMIT="25f6c0fadc51f65a0c05ca4540cb8699eba5c644"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
@@ -29,21 +29,26 @@ ffbuild_dockerbuild() {
         )
     fi
 
-    export CC="${FFBUILD_CROSS_PREFIX}gcc"
-    export CXX="${FFBUILD_CROSS_PREFIX}g++"
-    export LD="${FFBUILD_CROSS_PREFIX}gcc"
-    export AR="${FFBUILD_CROSS_PREFIX}ar"
-
     if [[ -n "$FFBUILD_RUST_TARGET" ]]; then
+        unset PKG_CONFIG_LIBDIR
+
+        export CC="gcc"
+        export CXX="g++"
+        export TARGET_CC="${FFBUILD_CROSS_PREFIX}gcc"
+        export TARGET_CXX="${FFBUILD_CROSS_PREFIX}g++"
+        export CROSS_COMPILE=1
+        export TARGET_CFLAGS="$CFLAGS"
+        export TARGET_CXXFLAGS="$CFLAGS"
+        unset CFLAGS
+        unset CXXFLAGS
+
         myconf+=(
             --target="$FFBUILD_RUST_TARGET"
         )
         cat <<EOF >$CARGO_HOME/config.toml
-[build]
-target = "$FFBUILD_RUST_TARGET"
 [target.$FFBUILD_RUST_TARGET]
-linker = "$LD"
-ar = "$AR"
+linker = "${FFBUILD_CROSS_PREFIX}gcc"
+ar = "${FFBUILD_CROSS_PREFIX}ar"
 EOF
     fi
 
