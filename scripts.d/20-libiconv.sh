679d6dc33594f36142b215c3a97d1c2acd0486db
rokibhasansagar <10854164+rokibhasansagar@users.noreply.github.com>
2023-04-24 19:25:07 +0600

    scripts.d/20-libiconv.sh: Permanently replace savannah with repo.or.cz
    Signed-off-by: rokibhasansagar <10854164+rokibhasansagar@users.noreply.github.com>

--- scripts.d/20-libiconv.sh
+++ scripts.d/20-libiconv.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://git.savannah.gnu.org/git/libiconv.git"
-SCRIPT_COMMIT="c593e206b2d4bc689950c742a0fb00b8013756a0"
-SCRIPT_REPO2="https://repo.or.cz/libiconv.git"
-SCRIPT_BRANCH2="master"
+SCRIPT_REPO1="https://repo.or.cz/libiconv.git"
+SCRIPT_REPO2="https://repo.or.cz/gnulib.git"
+SCRIPT_BRANCH="master"
 
 ffbuild_enabled() {
     return 0
 }
 
 ffbuild_dockerbuild() {
-    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" iconv || {
-        rm -rf iconv 2>/dev/null
-        git-mini-clone "$SCRIPT_REPO2" "${SCRIPT_BRANCH2}" iconv
-    }
+    git-mini-clone "$SCRIPT_REPO1" "${SCRIPT_BRANCH}" iconv
     cd iconv
-    git checkout "$SCRIPT_COMMIT"
 
-    ./autopull.sh --one-time || {
-        rm -rf gnulib 2>/dev/null
-        git-mini-clone "https://repo.or.cz/gnulib.git" "master" "gnulib"
-    }
+    git-mini-clone "$SCRIPT_REPO2" "${SCRIPT_BRANCH}" gnulib
+
     ./autogen.sh
 
     local myconf=(
